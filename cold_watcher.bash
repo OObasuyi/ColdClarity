fc77cf2b31d623bb36c32371bb5af9915f11f841
oobasuyi <80005871+OObasuyi@users.noreply.github.com>
2023-09-25 11:56:14 +0200

    add daemon to make sure container runs and cleans up all containers

---
+++ cold_watcher.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# made for RHEL PODMAN but can be switched for DOCKER
+CONTAINER_NAME="coldclarity"
+CONFIG_LOCATION="/path/to/config"
+CERT_PFX_LOCATION="PATH/TO/CERT"
+MAX_RETRIES=3
+RETRIES=0
+
+# IF YOU DONT USE CERT DROP "-v "$CERT_PFX_LOCATION":/ColdClarity/certificate_information/ise_client_cert.pfx:Z" from the command line
+start_container() {
+    podman run -it -v "$CONFIG_LOCATION":/ColdClarity/Config_information/config.yaml:Z -v "$CERT_PFX_LOCATION":/ColdClarity/certificate_information/ise_client_cert.pfx:Z "$CONTAINER_NAME"
+}
+
+cleanup_containers() {
+    # List all containers, including stopped ones
+    container_list=$(podman ps -a -q)
+
+    if [ -n "$container_list" ]; then
+        # Remove all stopped containers
+        podman rm $container_list
+    fi
+}
+
+# Trap exit signal to ensure cleanup even on script interruption
+trap cleanup_containers EXIT
+
+while true; do
+    start_container
+
+    # Check if the container is running
+    if podman container exists "$CONTAINER_NAME"; then
+        echo "ColdClarity is running."
+        break
+    else
+        # The container failed to start
+        echo "ColdClarity failed to start."
+
+        # Increment the retry counter
+        RETRIES=$((RETRIES + 1))
+
+        # Check if we've exceeded the maximum number of retries
+        if [ $RETRIES -ge $MAX_RETRIES ]; then
+            echo "Maximum number of retries reached. Exiting."
+            exit 1
+        else
+            echo "Retrying (Attempt $RETRIES)..."
+            sleep 5
+        fi
+    fi
+done
