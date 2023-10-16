9571944ee0892253f8ef30c008bce26cd44da04f
oobasuyi <80005871+OObasuyi@users.noreply.github.com>
2023-10-16 13:18:48 +0200

    added logging, made sure if we are not starting restart correctly.

--- cold_watcher.bash
+++ cold_watcher.bash
@@ -5,24 +5,39 @@ IMAGE_NAME="coldclarity"
 CONTAINER_NAME="ColdClaritySVCS_$RANDOM"
 CONFIG_LOCATION="/path/to/config"
 CERT_PFX_LOCATION="PATH/TO/CERT"
+WORKING_DIR="/opt/$IMAGE_NAME"
+LOGGING_FILE="$WORKING_DIR/logs/$IMAGE_NAME.log"
 MAX_RETRIES=3
 RETRIES=0
 
+# Script should run from the /OPT directory; configs can be anywhere
 # IF YOU DONT USE CERT DROP "-v "$CERT_PFX_LOCATION":/ColdClarity/certificate_information/ise_client_cert.pfx:Z" from the command line
+
+# Check or create logging Dir and file
+if [ ! -d "$WORKING_DIR/logs" ]; then
+    # If the directory doesn't exist, create it
+    mkdir -p "$WORKING_DIR/logs"
+    echo "$(date): $WORKING_DIR/logs created." >> "$LOGGING_FILE"
+else
+    echo "$(date): $WORKING_DIR/logs already exists." >> "$LOGGING_FILE"
+fi
+
+
 start_container() {
     podman run -d -v "$CONFIG_LOCATION":/ColdClarity/Config_information/config.yaml:Z -v "$CERT_PFX_LOCATION":/ColdClarity/certificate_information/ise_client_cert.pfx:Z --name "$CONTAINER_NAME" "$IMAGE_NAME"
 }
 
 cleanup_containers() {
-    podman rm "$CONTAINER_NAME"
-    echo "Removed STOPPED $CONTAINER_NAME"
+    podman rm -f "$CONTAINER_NAME"
+    echo "$(date): Removed STOPPED $CONTAINER_NAME" >> "$LOGGING_FILE"
 }
 
 # Trap exit signal to ensure cleanup even on script interruption
 trap cleanup_containers EXIT
 
 while true; do
-    start_container
+    init_container=$(start_container)
+    echo "$(date): $init_container" >> "$IMAGE_NAME started with $LOGGING_FILE"
 
      # Wait for the container to exit
     podman wait "$CONTAINER_NAME"
@@ -30,24 +45,47 @@ while true; do
     # Get the exit status of the container
     container_status=$(podman inspect -f "{{.State.ExitCode}}" "$CONTAINER_NAME")
 
-    # Check if the container exited gracefully (with an exit status of 0)
-    if [ "$container_status" -eq 0 ]; then
-        echo "ColdClarity has completed"
-        break
+
+    # check to make sure the container really started
+    container_ID_pat="^[0-9a-fA-F]{64}$"
+    if [[ $init_container =~ $container_ID_pat ]]; then
+          echo "$(date): $CONTAINER_NAME started successfully" >> "$LOGGING_FILE"
+
+          # Check if the container exited gracefully (with an exit status of 0)
+          if [ "$container_status" -eq 0 ]; then
+              echo "$(date): ColdClarity has completed" >> "$LOGGING_FILE"
+              break
+          else
+                 # if we failed because the container started but problem is from app
+                 echo "$(date): ColdClarity exited with an error (Exit Status: $container_status)." >> "$LOGGING_FILE"
+
+                 # Increment the retry counter
+                 RETRIES=$((RETRIES + 1))
+
+                # Check if we've exceeded the maximum number of retries
+                if [ $RETRIES -ge $MAX_RETRIES ]; then
+                    echo "$(date): Maximum number of retries reached. Exiting." >> "$LOGGING_FILE"
+                    exit 1
+                else
+                    echo "$(date): Retrying (Attempt $RETRIES)..." >> "$LOGGING_FILE"
+                    sleep 5
+                fi
+          fi
     else
-        # The container failed to start
-        echo "ColdClarity exited with an error (Exit Status: $container_status)."
+       echo "$(date): ERROR Container failed to start" >> "$LOGGING_FILE"
+       # Increment the retry counter
+       RETRIES=$((RETRIES + 1))
 
-        # Increment the retry counter
-        RETRIES=$((RETRIES + 1))
+      # Check if we've exceeded the maximum number of retries
+      if [ $RETRIES -ge $MAX_RETRIES ]; then
+          echo "$(date): Maximum number of retries reached. Exiting." >> "$LOGGING_FILE"
+          exit 1
+      else
+          echo "$(date): Retrying (Attempt $RETRIES)..." >> "$LOGGING_FILE"
+          sleep 5
+      fi
 
-        # Check if we've exceeded the maximum number of retries
-        if [ $RETRIES -ge $MAX_RETRIES ]; then
-            echo "Maximum number of retries reached. Exiting."
-            exit 1
-        else
-            echo "Retrying (Attempt $RETRIES)..."
-            sleep 5
-        fi
     fi
 done
+
+
